05ab5cff063b4c9c184c71bd902274d2dd5ba034
l36273442 <l36273442@163.com>
2023-04-23 16:41:29 +0800

    traceid

--- src/main/java/com/example/tools/ToolsApplication.java
+++ src/main/java/com/example/tools/ToolsApplication.java
@@ -4,14 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication
-
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,DataSourceTransactionManagerAutoConfiguration.class,})
 //@MapperScan({"com.example.tools.dao", "com.example.tools.dao2"})
+@SpringBootApplication
 public class ToolsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ToolsApplication.class, args);
     }
-
 }
